0289872410ebe948ece9034af9290e1cba733284
ep.Se.Iv <epam.serbin.ivan@gmail.com>
2022-05-31 01:10:11 +0300

    Enter operation check, enter number check

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,37 +11,21 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String args[]){
-        float a, b; //operands fields
-        float res;  //result field
-        char oper;  //operator field
+    public static void main(String[] args){
+        double a, b;    //operands
+        double res;     //result field
+        char oper;      //operator field
 
-        a=enter();  //Enter first float number
-
-        //Enter operation
-        for (;;){ // If you entered not the expected operator, the cycle prompts you to enter again
-            oper=operation();
-            if (oper=='+'){
-                break;
-            }else if (oper=='-'){
-                break;
-            }else if (oper=='*'){
-                break;
-            }else if (oper=='/'){
-                break;
-            }else {
-                System.out.println("You entered unexpected symbol, try again.");
-            }
-        }
-
-        b=enter();        //Enter second float number
-        res=calculate(a, oper, b); //Calculate result
+        a=enterNumber();            //Enter first double number
+        oper=enterOperation();      //Enter operation
+        b=enterNumber();            //Enter second double number
+        res=calculate(a, oper, b);  //Calculate result
         System.out.println("Result: "+a+oper+b+'='+res); //Output result
     }
 
     //Calculation method
-    private static float calculate (float a, char oper, float b){
-        float res=0;
+    private static double calculate (double a, char oper, double b){
+        double res=0;
         switch (oper) {
             case ('+'): {
                 res=a+b;
@@ -71,22 +55,39 @@ public class Calculator {
     }
 
     //Enter operation from console
-    private static char operation() {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter operation(+,-,*,/):");
-        return in.next().charAt(0);
-
+    private static char enterOperation() {
+        char oper;
+        for (;;){ // If you entered not the expected operator, the cycle prompts you to enter again
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter operation (+,-,*,/): ");
+            oper = in.next().charAt(0);
+            if (oper=='+'){
+                break;
+            }else if (oper=='-'){
+                break;
+            }else if (oper=='*'){
+                break;
+            }else if (oper=='/'){
+                break;
+            }else {
+                System.out.println("You entered unexpected symbol, try again.");
+            }
+        }
+        return oper;
     }
-    private static float enter() {                      //Enter number from console
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter a number:");
-        float num=0;
 
-        try {
-            num = in.nextFloat();
-        }catch (InputMismatchException e) {
-            System.out.println("Your input is not a number! Exit");
-            System.exit(1);
+    //Enter double number from console method
+    private static double enterNumber() {
+        double num;
+        while (true) {
+            try {
+                Scanner in = new Scanner(System.in);
+                System.out.print("Enter a number: ");
+                num = in.nextDouble();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Your input is not a number! Try again!");
+            }
         }
         return (num);
     }
