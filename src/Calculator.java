31092b94004fd4c4454b490a4c2004ceb54f9761
ep.Se.Iv <epam.serbin.ivan@gmail.com>
2022-06-03 09:20:06 +0300

    stable - use coma or dot, use 'c' to cancel

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,111 +1,56 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
-
-    public static void main(String[] args) throws Exception {
-        double num1, num2;              //operands
-        double result;                  //result field
-        char operation;      //operator field
-        System.out.println("Starting new calculation:");
-        num1 = enterDouble();             //Enter first double number
-        operation=enterOperation();                 //Enter operation
-        while (true) {                  //Catch "division by 0" Exception and enter new divider.
-            try {
-                num2 = enterDouble();            //Enter second double number
-                result = calculate(num1, operation, num2);  //Calculate result
-                break;
-            } catch (ArithmeticException e) { //Exception in Calculate method. "Division by 0. -> Enter another divider."
-                System.out.println(e.getMessage());
-            }
-        }
-        System.out.println("Result: " + num1 + operation + num2 + '=' + result); //Output result
+    public static void main(String[] args){
+        System.out.println("Start new calculation.");
+        calculate(enterDouble(), enterOperation());  //Calculate result
     }
 
     //Calculation method
-    private static double calculate (double a, char oper, double b) throws Exception{
-        switch (oper) {
-            case ('+') -> {return add(a, b);}
-            case ('-') -> {return subtract(a, b);}
-            case ('*') -> {return multiply(a, b);}
-            case ('/') -> {return divide(a, b);}
-            default -> throw new Exception("Unknown operation: \""+ oper+"\"");
+    private static void calculate (double a,char operation){
+        switch (operation) {
+                case ('+') -> a=a+enterDouble();
+                case ('-') -> a=a-enterDouble();
+                case ('*') -> a=a*enterDouble();
+                case ('/') -> {
+                        double divider = enterDouble();
+                        if (divider==0){
+                            System.out.println("Division by 0. Enter another divider: ");
+                            calculate (a, operation);
+                        }else {
+                            a=a/divider;
+                        }
+                }
+                case ('c') -> {
+                    System.out.println("Start new calculation.");
+                    a=enterDouble();
+                }
+                default -> {
+                    System.out.println("Unknown operation: \"" + operation + "\"");
+                    calculate (a, enterOperation());
+                }
         }
-    }
-
-    //Addition method
-    private static double add(double a, double b){
-        return a+b;
-    }
-
-    //Subtraction method
-    private static double subtract(double a, double b){
-        return a-b;
-    }
-
-    //Multiplication method
-    private static double multiply(double a, double b){
-        return a*b;
-    }
-
-    //Division method
-    private static double divide (double a, double divider) throws ArithmeticException{
-        if (divider==0){
-            throw new ArithmeticException("Division by 0. Enter another divider: ");
-        }
-        return a/divider;
+        System.out.println("Result: "+a);
+        calculate (a,enterOperation());
     }
 
     //Enter operation from console
     private static char enterOperation() {
-        char oper;
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter operation (+,-,*,/): ");
-        oper=in.next().charAt(0);
-        switch (oper){
-            case '+', '-', '*', '/' -> {return oper;}
-            default -> {
-                System.out.println("Unknown operation: "+oper+" -> Try again!");
-                return enterOperation();
-            }
-        }
+        System.out.print("Enter next operation (+,-,*,/) or 'c' to cancel: ");
+        return in.next().charAt(0);
     }
 
     //Enter double number from console method
     private static double enterDouble() {
-        double num;
-        Scanner in = new Scanner("-2.1");
-        String str = new String(in.nextLine());
-        //read minus
-        boolean isMinus=false;
-        if (str.charAt(0)=='-') {
-            isMinus = true;
-
-        }
-        //read numbers before dot or comma
-        int commaCount = 0;
-        for (int i=0; i<str.length();i++){
-            if (str.charAt(i)=='.'){
-                commaCount++;
-            }else if (str.charAt(i)==','){
-                commaCount++;
-            }
-            if ((!Character.isDigit(str.charAt(i)) && str.charAt(i) != '.' && str.charAt(i) != '-') || commaCount > 1) {
-                return false;
-            }
-        }
-        //read dot or coma
-        //read numbers after comma
-        //concatenate
-        //String to double
         try{
-
-                System.out.print("Enter a number: ");
-                return in.nextDouble();
-            }catch (InputMismatchException e) {
-                System.out.println("Your input is not a number! Try again!");
-                return enterDouble();
-            }
-
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter a number: ");
+            String str = in.nextLine().replace(',','.');
+            return Double.parseDouble(str);
+        }catch (NumberFormatException e) {
+            System.out.println("Your input is not a number! Try again!");
+            return enterDouble();
+        }
     }
 }
